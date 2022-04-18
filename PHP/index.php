f5bb925afe7aabc3188ab60dcf4743078eee1bb9
Fernando-Ibarra <fi94457@gmail.com>
2022-04-18 08:05:35 -0600

    Herencia Java y Php

---
+++ PHP/index.php
@@ -0,0 +1,12 @@
+<?php
+require_once('car.php');
+require_once('uberX.php');
+require_once('account.php');
+require_once('uberPool.php');
+
+$uberX = new UberX("CVB123",new Account("Andrés Herrera", "AND456"), "Toyota", "Yaris");
+$uberX->printDataCar();
+
+$uberPool = new UberPool("TYU567",new Account("Andrés Ferrán", "AND765"), "Toyota", "Corolla");
+$uberPool->printDataCar();
+?>
