0b628bb7aa5b2ef42bfdd76670cc197f9030e24d
Fernando-Ibarra <fi94457@gmail.com>
2022-04-19 07:14:45 -0600

    Herencia Python y JS

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,7 @@
 var car = new Car( "AW456", new Account("Andrés Herrera", "QWE234"))
 car.passenger = 4;
 car.printDataCar();
+
+var uberX = new UberX("AWS456", new Account("Andrés Ferrán","AN756"), "Toyota", "Yaris")
+uberX.passenger=4;
+uberX.printDataCar();
