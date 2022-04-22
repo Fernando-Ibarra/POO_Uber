b73d2cbbe85454ad9ffc32627c66356c44cf02ba
Fernando-Ibarra <fi94457@gmail.com>
2022-04-22 07:12:35 -0600

    Encapsulando en JAVA

--- Java/Main.java
+++ Java/Main.java
@@ -3,12 +3,12 @@ package Java;
 class Main{
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-        Car car = new Car("AMQ123", new Account("Andrés Herrera", "AND123"));
-        car.passenger = 4;
-        car.printDataCar();
+        UberX uberX = new UberX("AMQ123", new Account("Andrés Herrera", "AND123"), "Toyota", "Yaris");
+        uberX.setPassenger(4);
+        uberX.printDataCar();
 
-        Car car2 = new Car("QWE567", new Account("Andrea Herrera", "ANDA876"));
+        /*Car car2 = new Car("QWE567", new Account("Andrea Herrera", "ANDA876"));
         car2.passenger = 3;
-        car.printDataCar();
+        car.printDataCar();*/
     }
 }
